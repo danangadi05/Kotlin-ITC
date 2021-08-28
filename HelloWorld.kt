bc7321873c206d5eec1ea58282d1dee4daba9134
Danangadi26 <87925043+danangadi05@users.noreply.github.com>
2021-08-28 10:03:46 +0700

    Add files via upload

---
+++ HelloWorld.kt
@@ -0,0 +1,68 @@
+//syntax yang (//) untuk coment 1 baris kalo (/* dan */) untuk coment beberapa baris
+//contoh output pada kotlin
+/*fun main(){
+    println("hello world")
+//dibawah ini penggunaan syntax untuk operasi mtk
+    println(15 + 15)
+}*/
+
+//ini syntax untuk
+//notes:var untuk variable yang  diubah kalo val untuk yang tidak bisa diubah
+/*fun main(){
+    var name:String="Danang"
+    name="adi"
+    val age :Int=15
+
+    println(name)
+    println(age)
+}*/
+
+//Display Variable
+/*fun main(){
+    val name= "john"
+    println( "hello " + name)
+}*/
+
+//Mulai dari sini belajar nya
+/*fun main(){
+
+    val number1 : Int = 17
+    val number2 : Int = 20
+
+    if (number1 > 10 && number2 > 25 ){
+        print ("nilai bagus")
+    } else {
+        print ("nilai jelek")
+    }
+
+}
+
+fun main (){
+    val number : Int = 20
+    val number2 : Int = 15
+
+    print("Angka pertama : $number , Angka kedua : $number2")
+}
+
+fun main (){
+    val number : Int = 60
+    val number2 : Int = 50
+
+    print ("Jumlah : $number + $number2 = ${number + number2}" )
+
+}
+
+fun main (){
+    var name : String = "Danang adi"
+    var Char : Char = 'a'
+
+    print(name[1])
+}
+
+
+fun main (){
+    val name : String? = "danang"
+    val textlength = name!!.length
+
+    println(textlength)
+}*/
